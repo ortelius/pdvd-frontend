15da80ded8576c01f6788b62a1d20b3994c60320
Steve Taylor <steve@deployhub.com>
2026-01-02 13:10:24 -0700

    fix compile err Signed-off-by: Steve Taylor <steve@deployhub.com>

--- lib/auth/index.ts
+++ lib/auth/index.ts
@@ -1,5 +1,5 @@
 // lib/auth/index.ts
 
-export { AuthProvider, User, LoginCredentials } from './AuthProvider.interface'
+export type { AuthProvider, User, LoginCredentials } from './AuthProvider.interface'
 export { RestAuthProvider } from './RestAuthProvider'
 export { createAuthProvider } from './factory'
